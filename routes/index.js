a0ec175878ef3212f8528807b566dfe644593957
Christopher Moura <ccmoura@inf.ufpel.edu.br>
2020-04-06 02:04:21 -0300

    ejs include

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,72 @@
 const conn = require("./../inc/db");
 var express = require("express");
+var menus = require("../inc/menus");
+var reservations = require("./../inc/reservations")
+var contacts = require('./../inc/contacts')
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  conn.query(`select * from tb_menus order by title`, (err, results) => {
-    if (err) {
-      console.log(err);
-    }
-    res.render("index", { title: "Restaurante Saboroso!", menus: results });
+  menus.getMenus().then(results => {
+    res.render("index", { title: "Restaurante Saboroso!", menus: results, isHome: true });
   });
 });
 
 router.get("/contacts", function(req, res, next) {
-  res.render("contacts", {
-    title: "Contato - Restaurante Saboroso!",
-    background: "images/img_bg_3.jpg",
-    h1: "Diga olá!"
-  });
+  contacts.render(req, res);
+});
+router.post("/contacts", function(req, res, next) {
+    if(!req.body.name){
+      contacts.render(req, res, 'Digite o nome');
+    } else if(!req.body.email){
+      contacts.render(req, res, 'Digite o email');
+    } else if(!req.body.message){
+      contacts.render(req, res, 'Digite a mensagem');
+    } else{
+      contacts.save(req.body).then(results => {
+        req.body = {}
+        contacts.render(req, res, null, 'Contato enviado com sucesso')
+      }).catch(err => {
+        contacts.render(req, res, err.message)
+      })
+    }
 });
 router.get("/menus", function(req, res, next) {
-  res.render("menus", {
-    title: "Menus - Restaurante Saboroso!",
-    background: "images/img_bg_1.jpg",
-    h1: "Saboreie nosso menu!"
+  menus.getMenus().then(results => {
+    res.render("menus", {
+      title: "Menus - Restaurante Saboroso!",
+      background: "images/img_bg_1.jpg",
+      h1: "Saboreie nosso menu!",
+      menus: results
+    });
   });
 });
 
 router.get("/reservations", function(req, res, next) {
-  res.render("reservations", {
-    title: "Reservas - Restaurante Saboroso!",
-    background: "images/img_bg_2.jpg",
-    h1: "Reserve uma Mesa!"
-  });
+  reservations.render(req, res)
+
+});
+
+router.post("/reservations", function(req, res, next) {
+  if(!req.body.name){
+    reservations.render(req, res, "Digite um nome")
+  } else if(!req.body.email){
+    reservations.render(req, res, "Digite o email")
+  } else if(!req.body.people){
+    reservations.render(req, res, "Selecione o número de pessoas")
+  } else if(!req.body.date){
+    reservations.render(req, res, "Escolha uma data")
+  } else if(!req.body.time){
+    reservations.render(req, res, "Escolha um horário")
+  } else{
+    reservations.save(req.body).then(results => {
+      req.body = {};
+      reservations.render(req, res, null, "Reserva realizada com sucesso!")
+    }).catch(err => {
+      reservations.render(req, res, err.message)
+    })
+  }
+  
 });
 
 router.get("/services", function(req, res, next) {
@@ -42,4 +76,6 @@ router.get("/services", function(req, res, next) {
     h1: "É um prazer poder servir!"
   });
 });
+
+
 module.exports = router;
